f50046cd32a0e78904ad4d2b76437846cfa1febe
H-P-Wilson <84135203+h-p-wilson@users.noreply.github.com>
2022-01-18 20:02:58 +0000

    Update PlantTracker.java

--- PlantTracker.java
+++ PlantTracker.java
@@ -7,8 +7,27 @@ public class PlantTracker {
 		plants= new ArrayList<Plant>();
 	}
 	
-	public boolean addPlants(String name, String latinName, String date) {
-		System.out.println("Adding a plant");
-		return true;
+	public boolean addPlants(String name, String latinName, String date, String care) {
+		if (name != null && latinName != null && date != null && care != null) {
+			Plant plant = new Plant(name, latinName, date, care);
+			plants.add(plant);
+			return true;
+		}
+		return false;
 	}
+	
+	public String[] getPlantInfo() {
+		if (plants.size()>0 ) {
+			String plantList[] = new String[plants.size()];
+			for (int i = 0; i < plantList.length; i++) {
+				Plant plant = plants.get(i);
+				String str = plant.toString();
+				plantList[i] = str;
+			}
+			return plantList;
+		} else {
+			return null;
+		}
+	}
+	
 }
