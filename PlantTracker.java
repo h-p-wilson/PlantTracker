11a9177a9996a19d0aee958d8ed59cf4c67c98a8
H-P-Wilson <84135203+h-p-wilson@users.noreply.github.com>
2022-01-18 19:13:16 +0000

    Update PlantTracker.java

--- PlantTracker.java
+++ PlantTracker.java
@@ -1,4 +1,14 @@
+import java.util.ArrayList;
 
 public class PlantTracker {
-
+	private static ArrayList<Plant> plants;
+	
+	public PlantTracker(){ 
+		plants= new ArrayList<Plant>();
+	}
+	
+	public boolean addPlants(String name, String latinName, String date) {
+		System.out.println("Adding a plant");
+		return true;
+	}
 }
